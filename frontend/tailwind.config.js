4bb5e0af2ec200256073f49fef652ff06e4cfec0
Rushiraj Kudapane <kudapanerushi13@gmail.com>
2024-01-26 12:58:34 +0530

    added map iteration for cards and changed styling of footer

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,13 @@ const withMT = require("@material-tailwind/react/utils/withMT");
 module.exports = withMT({
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      poppins: ["Poppins", "sans-serif"],
+      spacing: {
+        110: "50rem",
+        85: "21rem",
+      },
+    },
   },
   plugins: [],
 });
